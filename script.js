f4ef90bb4c98f6c1003ea35421680485f08ea1ca
Klaudia <gora.klaudia@gmail.com>
2018-08-13 14:54:18 +0200

    Changing to tmpl function.

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
         type: 'get',
         success: function(data) {
             $(data).each(function (index, value) {
-                $('.container').append('<li><div class="postTitle">'+ value.title +'</div>'+ value.body +'</li> </br></br>');
+                $.tmpl('<li><div class="postTitle"> ${title} </div> ${body} </li> </br></br>', value).appendTo('.container');
+                // $('.container').append('<li><div class="postTitle">'+ value.title +'</div>'+ value.body +'</li> </br></br>');
             });
         }
     })
