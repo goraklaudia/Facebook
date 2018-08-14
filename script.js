4c3a1f68723f2238826e20807ad72cd2e60fb77f
Klaudia <gora.klaudia@gmail.com>
2018-08-14 12:11:58 +0200

    The template was moved to another file.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
     var postTemplate = $('#post').html();
     var output = $('.container');
-
+    
     $.ajax({
         url: 'https://jsonplaceholder.typicode.com/posts',
         dataType:'json',
         type: 'get',
         success: function(data) {
             $(data).each(function (index, value) {
-                var result = Mustache.render(postTemplate, value);
-                output.append(result);
-            });
+                $.get('/templates/postTemplate.mst', function(template){
+                    let result = Mustache.render(template, value);
+                    output.append(result);
+                });
+            }); 
         }
     })
 });
