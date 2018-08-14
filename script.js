29a8da1e36f13954e4bfc128ad4f59f4c86fcdcb
Klaudia <gora.klaudia@gmail.com>
2018-08-14 13:20:14 +0200

    Created function in script.js, moved $(document).ready() to html.

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-$(document).ready(function(){
-    var postTemplate = $('#post').html();
-    var output = $('.container');
+function getPosts() {
+    let output = $('.container');
     
     $.ajax({
         url: 'https://jsonplaceholder.typicode.com/posts',
         dataType:'json',
         type: 'get',
         success: function(data) {
-            $(data).each(function (index, value) {
-                $.get('/templates/postTemplate.mst', function(template){
+            $.get('/templates/postTemplate.mst', function(template){
+                $(data).each(function (index, value) {
                     let result = Mustache.render(template, value);
                     output.append(result);
                 });
             }); 
         }
     })
-});
+}
