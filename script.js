bcc34f1c78b0b13342e033f65939003cc856b5b8
Klaudia <gora.klaudia@gmail.com>
2018-08-14 08:18:05 +0200

    Added mustache template

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
+    var postTemplate = $('#post').html();
+    var output = $('.container');
+
     $.ajax({
         url: 'https://jsonplaceholder.typicode.com/posts',
         dataType:'json',
         type: 'get',
         success: function(data) {
             $(data).each(function (index, value) {
-                $.tmpl('<li><div class="postTitle"> ${title} </div> ${body} </li> </br></br>', value).appendTo('.container');
-                // $('.container').append('<li><div class="postTitle">'+ value.title +'</div>'+ value.body +'</li> </br></br>');
+                var result = Mustache.render(postTemplate, value);
+                output.append(result);
             });
         }
     })
