2bdf36dd5c1df4cf7a638a9200fa11748ccf0f8e
Klaudia <gora.klaudia@gmail.com>
2018-08-20 15:57:06 +0200

    Changing from es6 to es5

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-import {Post} from './modules/post.js';
+function successGet(data) {
+        let output = $('.container');
+        console.log(this);
+            $.get('/templates/postTemplate.mst', function(template){
+                $(data).each(function (index, value) {
+                    var result = Mustache.render(template, value);
+                    output.append(result);
+                });
+            }); 
 
-let post = new Post();
-post.getPost();
+};
+   
+function getPost() {
+        $.ajax({
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            dataType:'json',
+            type: 'get',
+            success: function (data){
+                this.data1 = data;
+                successGet(this.data1);
+            }
+        });
+};
+
+getPost();
