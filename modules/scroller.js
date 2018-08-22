6b8c61414124fe5a52ad02ad25e1fec0827fc6cf
goraklaudia <gora.klaudia@gmail.com>
2018-08-22 08:50:11 +0200

    #9 adding math.floor

--- modules/scroller.js
+++ modules/scroller.js
@@ -3,16 +3,14 @@ import {Post} from '../modules/post.js';
 export class Scroller {
 
     loadPost() {
-        $(document).ready(function() {
             let post = new Post();
             post.get();
         
             $(window).scroll(function() {
-                if ($(document).height() - $(window).height() == $(window).scrollTop()) {
+                if (($(document).height() - $(window).height() == Math.floor($(window).scrollTop())) || ($(document).height() - $(window).height() == ((Math.floor($(window).scrollTop())+1)))) {
                         post.get();
                 }
             });
-        });
     }
 
 }
